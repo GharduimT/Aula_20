5b1f26eac294fbe0c2ff0c3493d850123b40e2c8
GharduimT <Gabriel.harduim.ti@gmail.com>
2025-06-17 21:35:00 -0300

    CONSEGUI ALGUMA COISA

--- Aula_20/atv_estelionato_1.py
+++ Aula_20/atv_estelionato_1.py
@@ -4,18 +4,19 @@ import pandas as pd
 
 #  Obtendo dados
 try:
+    print("Obtendo dados...")
     ENDERECO_DADOS = "https://www.ispdados.rj.gov.br/Arquivos/BaseDPEvolucaoMensalCisp.csv"
 
     df_ocorrencias = pd.read_csv(ENDERECO_DADOS, sep=';', encoding='iso-8859-1')
     
     #  Demilitando somente as variáveis do estlionato
-    df_estelionato = df_ocorrencias[['munic', 'estelionato']]
+    df_ocorrencias = df_ocorrencias[['munic', 'estelionato']]
 
     #  Totalizar as ocorrencias de ESTELIONATO por município(agrupar e somar)
     df_estelionato = df_ocorrencias.groupby('munic').sum(['estelionato']).reset_index()
 
     # O head() vai printar as primeiras linhas pra saber se oobtee os dados corretamente
-    
+    print( df_estelionato.head())
 except Exception as e:
     print(f"Erro ao obter dados: {e}")
     exit()
--- Aula_20/tempCodeRunnerFile.py
+++ Aula_20/tempCodeRunnerFile.py
@@ -1,10 +1,16 @@
+#import matplotlib.pyplot as plt
+import pandas as pd
+#import numpy as np
+
+#  Obtendo dados
 try:
+    print("Obtendo dados...")
     ENDERECO_DADOS = "https://www.ispdados.rj.gov.br/Arquivos/BaseDPEvolucaoMensalCisp.csv"
 
     df_ocorrencias = pd.read_csv(ENDERECO_DADOS, sep=';', encoding='iso-8859-1')
     
     #  Demilitando somente as variáveis do estlionato
-    df_estelionato = df_ocorrencias[['munic','estelionato']]
+    df_ocorrencias = df_ocorrencias[['munic', 'estelionato']]
 
     #  Totalizar as ocorrencias de ESTELIONATO por município(agrupar e somar)
     df_estelionato = df_ocorrencias.groupby('munic').sum(['estelionato']).reset_index()
